b910efced8b0b4d67415e29014d481b1bc40e26f
Timofey <nsk.timka2077@gmail.com>
2023-06-29 23:06:14 +0200

    added contacts

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-var loadMoreBtn = document.getElementById('load-more-btn');
-var content = document.getElementById('content');
-var newContent = document.getElementById('new-content');
+// var loadMoreBtn = document.getElementById('load-more-btn');
+// var content = document.getElementById('content');
+// var newContent = document.getElementById('new-content');
 
-loadMoreBtn.onclick = function() {
-  if (newContent.style.display === 'none') {
-    newContent.classList.add('fade-in');
-    newContent.style.display = 'block';
-    content.style.marginBottom = '0';
+// loadMoreBtn.onclick = function() {
+//   if (newContent.style.display === 'none') {
+//     newContent.classList.add('fade-in');
+//     newContent.style.display = 'block';
+//     content.style.marginBottom = '0';
+//   } else {
+//     newContent.classList.add('fade-out');
+//     setTimeout(function() {
+//       newContent.classList.remove('fade-out');
+//       newContent.style.display = 'none';
+//       content.style.marginBottom = '20px';
+//     }, 500);
+//   }
+// };
+
+const loadMoreBtn = document.getElementById("load-more-btn");
+const newContent = document.getElementById("new-content");
+
+let isNewContentVisible = false;
+
+loadMoreBtn.addEventListener("click", function () {
+  if (isNewContentVisible) {
+    newContent.style.opacity = "0";
+    newContent.style.transform = "translateY(-20px)";
+    loadMoreBtn.style.background = "white";
+    setTimeout(function () {
+      newContent.style.display = "none";
+      loadMoreBtn.style.transform = "translateY(0)";
+    }, 300);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   } else {
-    newContent.classList.add('fade-out');
-    setTimeout(function() {
-      newContent.classList.remove('fade-out');
-      newContent.style.display = 'none';
-      content.style.marginBottom = '20px';
-    }, 500);
+    newContent.style.display = "block";
+    setTimeout(function () {
+      newContent.style.opacity = "1";
+      newContent.style.transform = "translateY(0)";
+      loadMoreBtn.style.transform = "translate(250%, 400%)";
+      loadMoreBtn.style.background = "lightblue";
+      newContent.scrollIntoView({ behavior: "smooth" });
+    }, 0);
   }
-};
+
+  isNewContentVisible = !isNewContentVisible;
+});
