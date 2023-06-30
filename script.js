58b13254c29279107d3735d1421936cd6243633f
Timofey <nsk.timka2077@gmail.com>
2023-06-30 11:35:45 +0200

    some changes

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// var loadMoreBtn = document.getElementById('load-more-btn');
-// var content = document.getElementById('content');
-// var newContent = document.getElementById('new-content');
 
-// loadMoreBtn.onclick = function() {
-//   if (newContent.style.display === 'none') {
-//     newContent.classList.add('fade-in');
-//     newContent.style.display = 'block';
-//     content.style.marginBottom = '0';
-//   } else {
-//     newContent.classList.add('fade-out');
-//     setTimeout(function() {
-//       newContent.classList.remove('fade-out');
-//       newContent.style.display = 'none';
-//       content.style.marginBottom = '20px';
-//     }, 500);
-//   }
-// };
 
 const loadMoreBtn = document.getElementById("load-more-btn");
 const newContent = document.getElementById("new-content");
+var footer = document.getElementById("footer");
 
 let isNewContentVisible = false;
 
