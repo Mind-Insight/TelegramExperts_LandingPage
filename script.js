ce277b91fc95bef47e651fc25e3b537dede22840
Timofey <nsk.timka2077@gmail.com>
2023-06-30 14:51:29 +0200

    some changes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const loadMoreBtn = document.getElementById("load-more-btn");
 const newContent = document.getElementById("new-content");
 var footer = document.getElementById("footer");
@@ -8,24 +6,16 @@ let isNewContentVisible = false;
 
 loadMoreBtn.addEventListener("click", function () {
   if (isNewContentVisible) {
-    newContent.style.opacity = "0";
-    newContent.style.transform = "translateY(0)";
-    loadMoreBtn.style.background = "white";
-    setTimeout(function () {
-      newContent.style.display = "none";
-      loadMoreBtn.style.transform = "translateY(0)";
-    }, 300);
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    newContent.scrollIntoView({ behavior: "smooth" });
   } else {
     newContent.style.display = "block";
     setTimeout(function () {
       newContent.style.opacity = "1";
       newContent.style.transform = "translateY(0)";
-      loadMoreBtn.style.transform = "translate(250%, 400%)";
       loadMoreBtn.style.background = "lightblue";
       newContent.scrollIntoView({ behavior: "smooth" });
     }, 0);
   }
 
-  isNewContentVisible = !isNewContentVisible;
+  isNewContentVisible = true;
 });
